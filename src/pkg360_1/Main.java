f77f392a472f0112f1d53e6bb2c3531e3e2b5fd8
Abdul Ashwood <aashwood@aggies.ncat.edu>
2018-09-16 21:33:59 -0400

    9/16/2018

--- src/pkg360_1/Main.java
+++ src/pkg360_1/Main.java
@@ -4,8 +4,11 @@
  * and open the template in the editor.
  */
 package pkg360_1;
-import java.io.*; 
+
+import java.io.*;
 import java.util.*;
+import javax.swing.*;
+
 /**
  *
  * @author Abdul
@@ -15,48 +18,97 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    ArrayList<String> lexemes = new ArrayList<String>();
+    ArrayList<String> tokens = new ArrayList<String>();
+
     public static void main(String[] args) {
         // TODO code application logic here
-        
-       try{
-       
-           File file = new File("360_txt.txt"); 
-           Scanner sc = new Scanner(file);
-           sc.nextLine();
-          String datatype = sc.next();
-          
-          if(datatype.equalsIgnoreCase("void")){
-          
-          }else if(datatype.equals("double") || datatype.equals("int") || datatype.equals("char")|| datatype.equals("String") || datatype.equals("float") || datatype.equals("long")){
-          
-          }
-           
-           System.out.println("ok");
-           
-           
-           
-           
-       }catch(Exception e) {
-       System.out.println("error");
-       }
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
+        Main r = new Main();
+
+        try {
+
+            File file = new File("360_txt.txt");
+            Scanner sc = new Scanner(file);
+
+            String datatype = sc.nextLine();
+            while (sc.hasNext()) {
+                datatype = sc.nextLine();
+                int t = r.isDataType(datatype);
+
+                //System.out.println(datatype);
+            }
+
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+
     }
-    
+
+    public void isVariable(String x) {
+        if (x.equals("double") || x.equals("int") || x.equals("char")
+                || x.equals("String") || x.equals("float") || x.equals("long")) {
+            System.out.print("<ident> ");
+        } else {
+            System.out.print("<var> ");
+        }
+    }
+
+    public int isDataType(String x) {
+        int n = 0;
+        String[] u = x.split(" ");
+        for (int i = 0; i < u.length; i++) {
+            System.out.println(u[i]);
+        }
+       /* for (int i = 0; i < u.length; i++) {
+            if (u[i].contains("float")) {
+                System.out.print("<ident> ");
+                i++;
+                isVariable(u[i]);
+            }
+        }*/
+
+        if (x.contains(
+                "double")) {
+            lexemes.add("double");
+            n++;
+        }
+
+        if (x.contains(
+                "int")) {
+            lexemes.add("int");
+            n++;
+        }
+
+        if (x.contains(
+                "char")) {
+            lexemes.add("char");
+            n++;
+        }
+
+        if (x.contains(
+                "string")) {
+            lexemes.add("String");
+            n++;
+        }
+
+        if (x.contains(
+                "float")) {
+            lexemes.add("float");
+            n++;
+        }
+
+        if (x.contains(
+                "long")) {
+            lexemes.add("long");
+            n++;
+        }
+
+        if (n > 1) {
+            JOptionPane.showMessageDialog(null, "will not run 001");
+        }
+
+        return n;
+    }
+
 }
+
