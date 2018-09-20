19d28648e443238ed8e49aeadf6d6ba57fd988ea
Robniqua Wilder-Winborne <rlwilder@aggies.ncat.edu>
2018-09-20 18:45:00 -0400

    print methods
    Nice neat printing methods to display the tokens and lexemes

---
+++ print.java
@@ -0,0 +1,15 @@
+public void print(){
+        operations.print(lexemes, tokens);
+    }
+
+    public static void print(ArrayList<String> lex, ArrayList<String> tok) {
+        String heading1 = "Lexemes";
+        String heading2 = "Tokens";
+        String divider = "-------------------------------";
+        System.out.printf( "%-15s %15s %n", heading1, heading2);
+        System.out.println(divider);
+        for (int i = 0; i < lex.size(); i++) {
+            System.out.printf( "%-15s %15s %n", lex.get(i), tok.get(i));
+        }
+        System.out.println(divider);
+    }
