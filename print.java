6dc6f5b81f251606af700c71c80d5fac304e2667
Robniqua Wilder-Winborne <rlwilder@aggies.ncat.edu>
2018-09-20 18:47:24 -0400

    edit
    Changing the alignment

--- print.java
+++ print.java
@@ -5,11 +5,11 @@ public void print(){
     public static void print(ArrayList<String> lex, ArrayList<String> tok) {
         String heading1 = "Lexemes";
         String heading2 = "Tokens";
-        String divider = "-------------------------------";
-        System.out.printf( "%-15s %15s %n", heading1, heading2);
+        String divider = "------------------------------------";
+        System.out.printf( "%-15s %-15s %n", heading1, heading2);
         System.out.println(divider);
         for (int i = 0; i < lex.size(); i++) {
-            System.out.printf( "%-15s %15s %n", lex.get(i), tok.get(i));
+            System.out.printf( "%-15s %-15s %n", lex.get(i), tok.get(i));
         }
         System.out.println(divider);
     }
